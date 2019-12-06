9cedf97b70a9259b378030f424c49c7fb8fc163a
croustibzh <m.ledot@outlook.com>
2019-12-06 10:56:33 -0500

    Dist unit switch button now changes constant through the app

--- app/src/main/java/com/georgebrown/comp2074/footmarker/Constants.java
+++ app/src/main/java/com/georgebrown/comp2074/footmarker/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static final int ERROR_DIALOG_REQUEST = 9001;
     private static final int PERMISSION_REQUEST_ENABLE_GPS=9002;
     private static final int PERMISSION_REQUEST_ACCESS_FINE_LOCATION =9003;
+    public static boolean DIST_UNIT = false;
 }
